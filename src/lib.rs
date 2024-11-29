df9c3705e0f58a68863ed73f4a92f472153878c6
Eric Wolf <robo-eric@gmx.de>
2024-11-29 19:07:00 +0100

    replace some associated types in traits with `async fn`
    This makes implementing the traits less painful.

--- src/lib.rs
+++ src/lib.rs
@@ -6,9 +6,8 @@
 
 pub mod backends;
 
-use futures::io::{AsyncRead, AsyncWrite, Error as IoError};
+use futures::io::{AsyncRead, AsyncWrite, Result as IoResult};
 use futures::stream::Stream;
-use std::future::Future;
 
 /// The client backend.
 /// 
@@ -19,56 +18,31 @@ pub trait ClientBackend {
     /// This can be for example a UUID.
     type ClientID;
 
-    /// Type representing data stored for a client.
-    /// 
-    /// See [`ClientBackend::client`].
-    type Client: AsyncRead;
-
-    /// Set of clients currently registered with the repository.
-    /// 
-    /// See [`ClientBackend::clients`].
-    type Clients: Stream<Item = Result<Self::ClientID, IoError>>;
-
-    /// Type representing data to be stored for a client.
-    /// 
-    /// See [`ClientBackend::add_client`].
-    type UploadFuture: AsyncWrite;
-
-    /// Future representing a request for client data.
-    /// 
-    /// See [`ClientBackend::client`].
-    type DownloadFuture: Future<Output = Result<Self::Client, IoError>>;
-
-    /// Future representing a client removal operation.
-    /// 
-    /// See [`ClientBackend::remove_client`]
-    type RemoveFuture: std::future::Future<Output = Result<(), IoError>>;
-
     /// Enumerate all clients currently registered with the repository.
     /// 
     /// Changes by adding or removing clients during enumeration may or may not
     /// be picked up.
-    fn clients(&self) -> Self::Clients;
+    async fn clients(&self) -> IoResult<impl Stream<Item = IoResult<Self::ClientID>>>;
 
     /// Request data associated with a client.
     /// 
     /// Each client can have associated data stored in the repository.
-    fn client(&self, id: &Self::ClientID) -> Self::DownloadFuture;
+    async fn client(&self, id: &Self::ClientID) -> IoResult<impl AsyncRead>;
 
     /// Register a client with the repository.
     /// 
-    /// The client will be registerd when the future is closed.
+    /// The client will be registerd when the async write is closed.
     /// 
     /// A client has to be registered with the repository before
     /// he can perform any operations on it.
     /// Should the client already be registered with the repository
     /// its associated data will be overwritten.
-    fn add_client(&self, id: &Self::ClientID) -> Self::UploadFuture;
+    async fn add_client(&self, id: &Self::ClientID) -> IoResult<impl AsyncWrite>;
 
     /// Remove a client from the repository.
     /// 
     /// Clients have to finish all pending operations before being removed from the repository.
-    fn remove_client(&self, id: &Self::ClientID) -> Self::RemoveFuture;
+    async fn remove_client(&self, id: &Self::ClientID) -> IoResult<()>;
 }
 
 /// A trait which allows fossils to give information about which chunk they represent.
@@ -122,79 +96,36 @@ pub trait ChunkBackend {
     /// See [`Fossil`].
     type Fossil: Fossil<ChunkID = Self::ChunkID>;
 
-    /// Type representing the contents of a chunk.
-    /// 
-    /// See [`ChunkBackend::chunk`].
-    type Chunk: AsyncRead;
-
-    /// Set of chunks currently stored in the repository.
-    /// 
-    /// See [`ChunkBackend::chunks`].
-    type Chunks: Stream<Item = Result<Self::ChunkID, IoError>>;
-
-    /// Type representing the contents of a chunk to be uploaded.
-    /// 
-    /// See [`ChunkBackend::add_chunk`].
-    type UploadFuture: AsyncWrite;
-
-    /// Future representing a query for chunk existence.
-    /// 
-    /// See [`ChunkBackend::has_chunk`].
-    type HasFuture: Future<Output = Result<bool, IoError>>;
-
-    /// Future representing a request for the contents of a chunk.
-    /// 
-    /// See [`ChunkBackend::chunk`].
-    type DownloadFuture: Future<Output = Result<Self::Chunk, IoError>>;
-
-    /// Set of fossils currently existing within the repository.
-    /// 
-    /// See [`ChunkBackend::fossils`].
-    type Fossils: Stream<Item = Result<Self::Fossil, IoError>>;
-
-    /// Future representing a request to turn a chunk into a fossil.
-    /// 
-    /// See [`ChunkBackend::make_fossil`].
-    type MakeFossilFuture: Future<Output = Result<Self::Fossil, IoError>>;
-
-    /// Future representing a request to turn a fossil back into a chunk.
-    /// 
-    /// See [`ChunkBackend::recover_fossil`].
-    type RecoverFuture: Future<Output = Result<(), IoError>>;
-
-    /// Future representing a request to delete a fossil.
-    /// 
-    /// See [`ChunkBackend::delete_fossil`].
-    type DeleteFuture: Future<Output = Result<(), IoError>>;
-
     /// Enumerate all chunks existing within the repository.
     /// 
     /// Changes by adding or removing chunks during enumeration may or may not
     /// be picked up.
-    fn chunks(&self) -> Self::Chunks;
+    async fn chunks(&self) -> IoResult<impl Stream<Item = IoResult<Self::ChunkID>>>;
 
     /// Request the contents of a chunk.
     /// 
     /// Its fossil can be used in case the original chunk does not exist.
-    fn chunk(&self, id: &Self::ChunkID) -> Self::DownloadFuture;
+    async fn chunk(&self, id: &Self::ChunkID) -> IoResult<impl AsyncRead>;
 
     /// Check whether a chunk exists in the repository.
     /// 
     /// The result of this query may be used to skip uploading chunks which
     /// already exist in the repository.
-    fn has_chunk(&self, id: &Self::ChunkID) -> Self::HasFuture;
+    async fn has_chunk(&self, id: &Self::ChunkID) -> IoResult<bool>;
 
     /// Store a chunk in the repository.
     /// 
+    /// The chunk will be added when the async write is closed.
+    /// 
     /// Should a chunk already exist within a repository its contents
     /// will be overwritten.
-    fn add_chunk(&self, id: &Self::ChunkID) -> Self::UploadFuture;
+    async fn add_chunk(&self, id: &Self::ChunkID) -> IoResult<impl AsyncWrite>;
 
     /// Enumerate all chunks existing within the repository.
     /// 
     /// Changes by fossilising chunks or recovering or deletion fossils during
     /// enumeration may or may not be picked up.
-    fn fossils(&self) -> Self::Fossils;
+    async fn fossils(&self) -> IoResult<impl Stream<Item = IoResult<Self::Fossil>>>;
 
     /// Turn a chunk into a fossil.
     /// 
@@ -206,13 +137,13 @@ pub trait ChunkBackend {
     /// 
     /// When the chunk does not exists this method should not return an error
     /// but instead treat the fossil as having been created and return its ID. 
-    fn make_fossil(&self, id: &Self::ChunkID) -> Self::MakeFossilFuture;
+    async fn make_fossil(&self, id: &Self::ChunkID) -> IoResult<Self::Fossil>;
 
     /// Turn a fossil back into a chunk.
     /// 
     /// When the fossil does not exist this method should not return an error
     /// but instead treat the chunk as having been restored.
-    fn recover_fossil(&self, id: &Self::Fossil) -> Self::RecoverFuture;
+    async fn recover_fossil(&self, id: &Self::Fossil) -> IoResult<()>;
 
     /// Delete a fossil permanently.
     /// 
@@ -222,7 +153,7 @@ pub trait ChunkBackend {
     /// 
     /// When the fossil does not exists this method should not return an error
     /// but instead treat the fossil as having been deleted successfully.
-    fn delete_fossil(&self, id: &Self::Fossil) -> Self::DeleteFuture;
+    async fn delete_fossil(&self, id: &Self::Fossil) -> IoResult<()>;
 }
 
 /// Data uploaded by a client, represented as a sequence of chunks.
@@ -237,9 +168,6 @@ pub trait Manifest {
     /// This can for example be a hash of its contents.
     type ChunkID;
 
-    /// Type representing the uploaded data as a sequence of chunks.
-    type Chunks: Stream<Item = Result<Self::ChunkID, IoError>>;
-
     /// Timestamp indicating the time of creation.
     /// 
     /// This is after all chunks where uploaded but may be before the manifest
@@ -250,7 +178,7 @@ pub trait Manifest {
     fn creator(&self) -> Self::ClientID;
 
     /// Enumerate the chunks this manifest is made of.
-    fn chunks(&self) -> Self::Chunks;
+    async fn chunks(&self) -> IoResult<impl Stream<Item = IoResult<Self::ChunkID>>>;
 }
 
 /// Trait representing a manifest creation process.
@@ -260,16 +188,6 @@ pub trait ManifestBuilder {
     /// This can for example be a hash of its contents.
     type ChunkID;
 
-    /// Future representing the process of adding a chunk to the manifest.
-    /// 
-    /// See [`ManifestBuilder::add_chunk`].
-    type ChunkFuture: Future<Output = Result<(), IoError>>;
-
-    /// Type representing additional data to store in the manifest.
-    /// 
-    /// See [`ManifestBuilder::add_data`].
-    type DataFuture: AsyncWrite;
-
     /// Add a chunk to the manifest file.
     /// 
     /// The chunk has to be added to the repository by the user before calling
@@ -278,15 +196,15 @@ pub trait ManifestBuilder {
     /// A backend can not depend on the list chunks being complete.
     /// Middlewares can encode additional chunks in the additional data or
     /// store the list as chunks themselves.
-    fn add_chunk(&mut self, id: &Self::ChunkID) -> Self::ChunkFuture;
+    async fn add_chunk(&mut self, id: &Self::ChunkID) -> IoResult<()>;
 
     /// Add additional data to the manifest.
     /// 
     /// Manifests can store custom data in addition to the sequence of its
     /// chunks, which allows clients to store (for example) additional metadata.
     /// 
-    /// When the returned future is closed the manifest will be created.
-    fn add_data(self) -> Self::DataFuture;
+    /// When the returned async write is closed the manifest will be created.
+    async fn add_data(self) -> IoResult<impl AsyncWrite>;
 }
 
 /// A repository storing clients, chunks and manifests.
@@ -304,43 +222,22 @@ pub trait Repository: ClientBackend + ChunkBackend {
         ChunkID = <Self as ChunkBackend>::ChunkID,
     >;
 
-    /// Type representing the set of manifests currently existing within the
-    /// repository.
-    /// 
-    /// See [`Repository::manifests`].
-    type Manifests: Stream<Item = Result<Self::ManifestID, IoError>>;
-
     /// Type representing a manifest being created.
     /// 
     /// See [`Repository::create_manifest`].
     type Builder: ManifestBuilder<ChunkID = <Self as ChunkBackend>::ChunkID>;
 
-    /// Future representing the query for manifest existence.
-    /// 
-    /// See [`Repository::has_manifest`].
-    type HasFuture: Future<Output = Result<bool, IoError>>;
-
-    /// Future representing the request for a manifest.
-    /// 
-    /// See [`Repository::manifest`].
-    type DownloadFuture: Future<Output = Result<Self::Manifest, IoError>>;
-
-    /// Future representing a manifest removal operation.
-    /// 
-    /// See [`Repository::remove_manifest`].
-    type RemoveFuture: Future<Output = Result<(), IoError>>;
-
     /// Enumerate all manifests existing within the repository.
     /// 
     /// Changes by creating or removing manifests during enumeration may or may
     /// not be picked up.
-    fn manifests(&self) -> Self::Manifests;
+    async fn manifests(&self) -> IoResult<impl Stream<Item = IoResult<Self::ManifestID>>>;
 
     /// Request a manifest.
-    fn manifest(&self, id: &Self::ManifestID) -> <Self as Repository>::DownloadFuture;
+    async fn manifest(&self, id: &Self::ManifestID) -> IoResult<Self::Manifest>;
 
     /// Check whether a manifest exists.
-    fn has_manifest(&self, id: &Self::ManifestID) -> <Self as Repository>::HasFuture;
+    async fn has_manifest(&self, id: &Self::ManifestID) -> IoResult<bool>;
 
     /// Create a manifest.
     /// 
@@ -353,5 +250,5 @@ pub trait Repository: ClientBackend + ChunkBackend {
     /// 
     /// Removing a manifest may leave unreferenced chunks behind, which is why
     /// this operation should be performed by a fossil collection step.
-    fn remove_manifest(&self, id: &Self::ManifestID) -> <Self as Repository>::RemoveFuture;
+    async fn remove_manifest(&self, id: &Self::ManifestID) -> IoResult<()>;
 }
